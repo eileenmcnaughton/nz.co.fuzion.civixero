9610e1b77e2b05d356f2cf0aacf4b9fac564c90e
Eileen McNaughton <eileen@fuzion.co.nz>
2015-02-19 11:23:20 +1300

    add exception on incorrectly formatted data from xero to itempull

--- CRM/Civixero/Item.php
+++ CRM/Civixero/Item.php
@@ -3,18 +3,27 @@
 class CRM_Civixero_Item extends CRM_Civixero_Base {
 
   /**
-   * pull Item Codes from Xero and temporarily stash them on static
-   * we don't want to keep stale ones in our DB - we'll check each time
-   * We call the civicrm_accountPullPreSave hook so other modules can alter if required
+   * Pull Item Codes from Xero and temporarily stash them on static.
+   *
+   * We don't want to keep stale ones in our DB - we'll check each time
+   * We call the civicrm_accountPullPreSave hook so other modules can alter
+   * if required.
+   *
    *  - I can't think of a reason why they would but it seems consistent
    *
    * @param array $params
-   * @throws API_Exception
+   *
+   * @return array
+   *
+   * @throws CRM_Core_Exception
    */
-  function pull($params) {
+  public function pull($params) {
     static $items = array();
-    if(empty($items)){
+    if (empty($items)) {
       $items = $this->getSingleton()->Items();
+      if (!is_array($items) || !is_array($items['Items'])) {
+        throw new CRM_Core_Exception('Items is not a valid array');
+      }
       $items = $items['Items']['Item'];
     }
     return $items;
