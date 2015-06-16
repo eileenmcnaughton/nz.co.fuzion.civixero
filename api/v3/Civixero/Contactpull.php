73165819248cf3e278edf950280964733737a2f6
Eileen <eileen@fuzion.co.nz>
2015-06-17 10:57:57 +1200

    Fix api to return success

--- api/v3/Civixero/Contactpull.php
+++ api/v3/Civixero/Contactpull.php
@@ -40,4 +40,5 @@ function _civicrm_api3_civixero_contactpull_spec(&$spec) {
 function civicrm_api3_civixero_contactpull($params) {
   $xero = new CRM_Civixero_Contact($params);
   $xero->pull($params);
+  return civicrm_api3_create_success(1, $params, 'Civixero', 'contactpull');
 }
