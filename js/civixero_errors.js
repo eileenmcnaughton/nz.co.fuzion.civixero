bc08206ea67fae7de6cfbe7815f2062a2e81c6e7
Alok Patel <alok@agileware.com.au>
2018-05-31 15:06:05 +0530

    CIVIXERO-11: Fixed sync errors on contact page to consider both object and array while showing alert message.

--- js/civixero_errors.js
+++ js/civixero_errors.js
@@ -5,6 +5,9 @@ CRM.$(function($) {
         $.getJSON(CRM.url("civicrm/ajax/civixero/sync/contact/errors",
             {xeroerrorid: $(this).data('xeroerrorid')})
         ).done(function (result) {
+            if((typeof result) == "object") {
+                result = getArrayFromObject(result);
+            }
             if(result.length > 0) {
                 CRM.alert(getErrorsText(result),"Contact sync","error");
             }
@@ -17,11 +20,23 @@ CRM.$(function($) {
         $.getJSON(CRM.url("civicrm/ajax/civixero/sync/invoice/errors",
             {xeroerrorid: $(this).data('xeroerrorid')})
         ).done(function (result) {
+            if((typeof result) == "object") {
+                result = getArrayFromObject(result);
+            }
             if(result.length > 0) {
                 CRM.alert(getErrorsText(result),"Contributions sync","error");
             }
         });
     });
+
+    function getArrayFromObject(object) {
+      var array = $.map(object, function(value, index) {
+        if(index != 'error_cleared') {
+          return [value];
+        }
+      });
+      return array;
+    }
 
     function getErrorsText(result) {
         var text ="";
