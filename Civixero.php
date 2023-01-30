49804eeff9273e029e6143078d1d988b570dcef6
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-01-30 13:19:02 +1300

    Remove functions broken by civix upgrade

--- Civixero.php
+++ Civixero.php
@@ -28,37 +28,10 @@ function civixero_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function civixero_civicrm_uninstall() {
-  _civixero_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function civixero_civicrm_enable() {
   _civixero_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function civixero_civicrm_disable() {
-  return _civixero_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue|null, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function civixero_civicrm_upgrade($op, ?CRM_Queue_Queue $queue = NULL) {
-  return _civixero_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
