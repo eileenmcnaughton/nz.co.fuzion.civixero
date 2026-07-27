92c06be04322b596e827534032e804d7eb46e36c
eileen <emcnaughton@wikimedia.org>
2026-07-27 14:37:52 +1200

    Minor tidy ups
    From Matt

--- CRM/Civixero/Invoice.php
+++ CRM/Civixero/Invoice.php
@@ -551,7 +551,7 @@ class CRM_Civixero_Invoice extends CRM_Civixero_Base {
     $accountInvoices = AccountInvoice::get(FALSE)
       ->addWhere('plugin', '=', 'xero')
       ->addWhere('connector_id', '=', $params['connector_id'])
-      ->addClause('OR', ['accounts_status_id', 'IS NULL'], ['accounts_status_id', 'NOT IN', [CRM_Core_PseudoConstant::getKey('CRM_Accountsync_BAO_AccountInvoice', 'accounts_status_id', 'cancelled')]])
+      ->addClause('OR', ['accounts_status_id', 'IS NULL'], ['accounts_status_id:name', 'NOT IN', ['cancelled']])
       ->addOrderBy('error_data')
       ->setLimit($limit);
 
@@ -802,6 +802,7 @@ class CRM_Civixero_Invoice extends CRM_Civixero_Base {
    * Get the Xero tax modes.
    *
    * This is accessed from the settings.
+   * See \XeroAPI\XeroPHP\Models\Accounting\LineAmountTypes
    *
    * @return array[]
    */
